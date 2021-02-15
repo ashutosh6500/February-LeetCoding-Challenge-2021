442dde325074fe8f1b73b66d4e3626dd9cef7e8a
Ashutosh Kodavade <ashutoshdkodavade@gmail.com>
2021-02-15 13:55:17 +0530

    The K Weakest Rows in a Matrix

---
+++ Feb_15.cpp
@@ -0,0 +1,55 @@
+/*
+Question:
+Given a m * n matrix mat of ones (representing soldiers) and zeros (representing civilians), return the indexes of the k weakest rows in the matrix ordered from the weakest to the strongest.
+
+A row i is weaker than row j, if the number of soldiers in row i is less than the number of soldiers in
+ row j, or they have the same number of soldiers but i is less than j. Soldiers are always stand in the frontier of
+  a row, that is, always ones may appear first and then zeros.
+Example 1:
+
+Input: mat =
+[[1,1,0,0,0],
+ [1,1,1,1,0],
+ [1,0,0,0,0],
+ [1,1,0,0,0],
+ [1,1,1,1,1]],
+k = 3
+Output: [2,0,3]
+Explanation:
+The number of soldiers for each row is:
+row 0 -> 2
+row 1 -> 4
+row 2 -> 1
+row 3 -> 2
+row 4 -> 5
+Rows ordered from the weakest to the strongest are [2,0,3,1,4]
+*/
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k)
+    {
+        multimap<int,int>occ;
+        int count=0,i=0;
+        for(auto t:mat)
+        {
+            for(auto e:t)
+            {
+                if(e==1)
+                    count++;
+            }
+            occ.insert({count,i});
+            i++;
+            count=0;
+        }
+        i=0;
+        vector<int>v;
+        for(auto y:occ)
+        {
+            if(i>=k)
+                break;
+            v.push_back(y.second);
+            i++;
+        }
+        return v;
+    }
+};
